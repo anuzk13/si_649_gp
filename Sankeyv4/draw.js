5a07a75334f3729177b102823e3108daa81897a5
Ana María Cárdenas Gasca <am.cardenas927@gmail.com>
2018-12-10 14:03:06 -0500

    Integrated charts modified functions to generalize plots

--- Sankeyv4/draw.js
+++ Sankeyv4/draw.js
@@ -1,9 +1,4 @@
-var data1 = []
-
-
-var cohortList = ["1", "2", "3", "4"]
-
-var legendLabels = ["0-25%", "26-50%", "51-75%", "76-100%"]
+var students_exams_data = []
 
 var color = d3.scaleOrdinal()
     .range(["#ca0020","#f4a582","#d5d5d5","#92c5de","#0571b0"]);
@@ -15,83 +10,57 @@ $(document).ready(function () {
 
 function loadData() {
     d3.csv("exams.csv", function (d) {
-        data1 = d
-        // createMChoiceCircles(findMChoiceItems(cohortFilter()));
-        // createMChoiceCircles(groupDataByChapterMChoice(["1","2"]));
-        // createParsonsCircles(groupDataByChapterParsons());
-        // createUnitTestCircles(groupDataByChapterUnitTest());
+        students_exams_data = d
     });
 
 };
 
-
 function cohortFilter(combos) {
-    var cohorts = data1.filter(function(d) { 
+    var cohorts = students_exams_data.filter(function(d) { 
         return combos.reduce((prev,current) => prev || d.combo_id == current, false)})
     return cohorts
-
 }
 
-function findMChoiceItems(cohorts) {
-    var items = cohorts.filter(function(d) {return d.event_name == "mchoice"})
+function findTestItems(cohorts, name) {
+    var items = cohorts.filter(function(d) {return d.event_name == name})
     return items;
 }
 
-function groupDataByChapterMChoice(combos) {
+function groupDataByChapter(test_cohorts) {
     var groupedData = d3.nest()
-        .key(function (d) { return d.div })
-        .entries(findMChoiceItems(cohortFilter(combos)));
-    return groupedData;
+        .key(function (d) { return +d.div_num })
+        .entries(test_cohorts);
+    return groupedData.sort((a,b) => a.key-b.key);
 }
 
-function findParsonsItems(cohorts) {
-    var items = cohorts.filter(function(d) {return d.event_name == "parsons"})
-    return items;
-}
+function createTestBars(filtered_cohorts, groups, test_name, test_id) {
 
-function groupDataByChapterParsons(combos) {
-    var groupedData = d3.nest()
-        .key(function (d) { return d.div })
-        .entries(findParsonsItems(cohortFilter(combos)));
-    return groupedData;
-}
-
-function findUnitTestItems(cohorts) {
-    var items = cohorts.filter(function(d) {return d.event_name == "unittest"})
-    return items;
-}
-
-function groupDataByChapterUnitTest(combos) {
-    var groupedData = d3.nest()
-        .key(function (d) { return d.div })
-        .entries(findUnitTestItems(cohortFilter(combos)));
-    return groupedData;
-}
-
-function createMChoiceCircles(dataitems) {
-
+    var test_cohorts = findTestItems(filtered_cohorts, test_name);
+    var dataitems = groupDataByChapter(test_cohorts);
+    var max = d3.max(test_cohorts, function(d) { return +d.student_q_count; } );
     var margin = { top: 20, right: 20, bottom: 30, left: 60 },
         width = 850 - margin.left - margin.right,
         height = 225 - margin.top - margin.bottom;
 
     var x0 = d3.scaleBand()
-        .domain(dataitems.map(function (d) { return d.key; }))
+        .domain(dataitems.map(function (d) { return "ch" + d.key; }))
         .rangeRound([0, width])
         .paddingInner(0.1);
 
     var x1 = d3.scaleBand()
-        .domain(cohortList)
+        .domain(groups)
         .rangeRound([0, x0.bandwidth()])
-        .padding(0.05);
+        .padding(0.1);
 
     var y = d3.scaleLinear()
-        // .domain([0, (d3.max(findMChoiceItems(cohortFilter()), function (d) { return d.student_q_count; }))])
-        .domain([0, 50])
+        .domain([0, max])
         .range([height, 0]);
 
-    var tooltip = d3.select("body").append("div").attr("class", "toolTip"); //NEW
+    var tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
-    var svg = d3.select("#mChoiceCircles").append("svg")
+    var svg = d3.select(test_id).append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
@@ -102,7 +71,7 @@ function createMChoiceCircles(dataitems) {
         .data(dataitems)
         .enter().append("g")
         .attr("class", "g")
-        .attr("transform",function(d) { return "translate(" + x0(d.key) + ",0)"; })
+        .attr("transform",function(d) { return "translate(" + x0("ch" + d.key) + ",0)"; })
 
     var rect = slice.selectAll(".rect")
         .data(function(d) { return d.values})
@@ -113,37 +82,59 @@ function createMChoiceCircles(dataitems) {
         .attr("y", function(d){ return y(d.student_q_count); })
         .attr("height", function(d){ return height - y(d.student_q_count); })
         .attr("fill", "#5b717c")
-        .on("mousemove", function(d,i){
+        .on("mouseover", function(d,i){
             d3.select(this)
                 .attr("stroke", "black")
                 .attr("stroke-width", 3);
-            tooltip.style("left", d3.event.pageX - 50 + "px")
-                .style("top", d3.event.pageY - 150 + "px")
-                .style("display", "inline-block")
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", .9);
+            tooltip
+                .style("left", (d3.event.pageX + 20) + "px")
+                .style("top", (d3.event.pageY - 70) + "px")
                 .html("<b>Students: </b>" + d.student_q_count + "<br>" + "<b>% Correct: </b>" + d.percent_correct + "<br>");
         })
         .on("mouseout", function(d){
             d3.select(this).attr("stroke-width", 0);
-            tooltip.style("display", "none");
+            // tooltip.style("display", "none");
         });
 
     // add the x Axis
     var gx0 = svg.append("g")
         .attr("class", "x_axis")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x0))
-        .selectAll("text")
+        .call(d3.axisBottom(x0));
+
+    gx0.selectAll("line")
+        .attr("y2", "20")
+        .attr("transform", "translate(20,0)")
+        .style("fill", "gray")
+
+    gx0.selectAll("text")
         .style("text-anchor", "end")
-        .attr("dx", "-.1em")
-        .attr("dy", "2em");
+        .style("fill", "gray")
+        .attr("dx", "0.6em")
+        .attr("dy", "2em")
+        .filter(function (d, i) { return i === 0;})
+        .style('fill', 'transparent');  
 
     // add the x Axis
     var gx1 = svg.append("g")
         .attr("class", "x1_axis")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x1))
-        .selectAll("text")
+        .call(d3.axisBottom(x1));
+
+    gx1.selectAll("line")
+        .style("stroke", "none")
+
+    gx1.selectAll(".domain")
+        .style("stroke", "none")
+
+    
+    gx1.selectAll("text")
         .style("text-anchor", "end")
+        .attr("transform", "rotate(-60)")
+        .style("fill","red")
         .attr("dx", "-.1em")
         .attr("dy", ".25em");
 
@@ -155,194 +146,11 @@ function createMChoiceCircles(dataitems) {
         .call(d3.axisLeft(y));
 
     svg.selectAll(".domain").style({"stroke-width": 0});
-
-
 }
 
-function createParsonsCircles(dataitems) {
+var heatmapChart = function(csvFile) {
 
-    var margin = { top: 20, right: 20, bottom: 30, left: 60 },
-        width = 450 - margin.left - margin.right,
-        height = 225 - margin.top - margin.bottom;
-
-    var x0 = d3.scaleBand()
-        .domain(dataitems.map(function (d) { return d.key; }))
-        .rangeRound([0, width])
-        .paddingInner(0.1);
-
-    var x1 = d3.scaleBand()
-        .domain(cohortList)
-        .rangeRound([0, x0.bandwidth()])
-        .padding(0.05);
-
-    var y = d3.scaleLinear()
-        // .domain([0, (d3.max(findMChoiceItems(cohortFilter()), function (d) { return d.student_q_count; }))])
-        .domain([0, 50])
-        .range([height, 0]);
-
-    var tooltip = d3.select("body").append("div").attr("class", "toolTip"); //NEW
-
-    var svg = d3.select("#parsonsCircles").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
-
-    var slice = svg.selectAll(".slice")
-        .data(dataitems)
-        .enter().append("g")
-        .attr("class", "g")
-        .attr("transform",function(d) { return "translate(" + x0(d.key) + ",0)"; })
-
-    var rect = slice.selectAll(".rect")
-        .data(function(d) { return d.values})
-        .enter().append("rect")
-        .attr("opacity", function(d) { return d.percent_correct / 100 })
-        .attr("x", function(d) { return x1(d.combo_id);})
-        .attr("width", x1.bandwidth())
-        .attr("y", function(d){ return y(d.student_q_count); })
-        .attr("height", function(d){ return height - y(d.student_q_count); })
-        .attr("fill", "#5b717c")
-        .on("mousemove", function(d,i){
-            d3.select(this)
-                .attr("stroke", "black")
-                .attr("stroke-width", 3);
-            tooltip.style("left", d3.event.pageX - 50 + "px")
-                .style("top", d3.event.pageY - 150 + "px")
-                .style("display", "inline-block")
-                .html("<b>Students: </b>" + d.student_q_count + "<br>" + "<b>% Correct: </b>" + d.percent_correct + "<br>");
-        })
-        .on("mouseout", function(d){
-            d3.select(this).attr("stroke-width", 0);
-            tooltip.style("display", "none");
-        });
-
-    // add the x Axis
-    var gx0 = svg.append("g")
-        .attr("class", "x_axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x0))
-        .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("dx", "-.1em")
-        .attr("dy", "2em");
-
-    // add the x Axis
-    var gx1 = svg.append("g")
-        .attr("class", "x1_axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x1))
-        .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("dx", "-.1em")
-        .attr("dy", ".25em");
-
-    // hide axis lines and ticks
-    svg.selectAll(".tick").style({"stroke-width":0});
-
-    // add the y Axis
-    var gy = svg.append("g")
-        .call(d3.axisLeft(y));
-
-    svg.selectAll(".domain").style({"stroke-width": 0});
-
-
-}
-
-function createUnitTestCircles(dataitems) {
-
-    var margin = { top: 20, right: 20, bottom: 30, left: 60 },
-        width = 450 - margin.left - margin.right,
-        height = 225 - margin.top - margin.bottom;
-
-    var x0 = d3.scaleBand()
-        .domain(dataitems.map(function (d) { return d.key; }))
-        .rangeRound([0, width])
-        .paddingInner(0.1);
-
-    var x1 = d3.scaleBand()
-        .domain(cohortList)
-        .rangeRound([0, x0.bandwidth()])
-        .padding(0.05);
-
-    var y = d3.scaleLinear()
-        // .domain([0, (d3.max(findMChoiceItems(cohortFilter()), function (d) { return d.student_q_count; }))])
-        .domain([0, 50])
-        .range([height, 0]);
-
-    var tooltip = d3.select("body").append("div").attr("class", "toolTip"); //NEW
-
-    var svg = d3.select("#unitTestCircles").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
-
-    var slice = svg.selectAll(".slice")
-        .data(dataitems)
-        .enter().append("g")
-        .attr("class", "g")
-        .attr("transform",function(d) { return "translate(" + x0(d.key) + ",0)"; })
-
-    var rect = slice.selectAll(".rect")
-        .data(function(d) { return d.values})
-        .enter().append("rect")
-        .attr("opacity", function(d) { return d.percent_correct / 100 })
-        .attr("x", function(d) { return x1(d.combo_id);})
-        .attr("width", x1.bandwidth())
-        .attr("y", function(d){ return y(d.student_q_count); })
-        .attr("height", function(d){ return height - y(d.student_q_count); })
-        .attr("fill", "#5b717c")
-        .on("mousemove", function(d,i){
-            d3.select(this)
-                .attr("stroke", "black")
-                .attr("stroke-width", 3);
-            tooltip.style("left", d3.event.pageX - 50 + "px")
-                .style("top", d3.event.pageY - 150 + "px")
-                .style("display", "inline-block")
-                .html("<b>Students: </b>" + d.student_q_count + "<br>" + "<b>% Correct: </b>" + d.percent_correct + "<br>");
-        })
-        .on("mouseout", function(d){
-            d3.select(this).attr("stroke-width", 0);
-            tooltip.style("display", "none");
-        });
-
-    // add the x Axis
-    var gx0 = svg.append("g")
-        .attr("class", "x_axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x0))
-        .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("dx", "-.1em")
-        .attr("dy", "2em");
-
-    // add the x Axis
-    var gx1 = svg.append("g")
-        .attr("class", "x1_axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x1))
-        .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("dx", "-.1em")
-        .attr("dy", ".25em");
-
-    // hide axis lines and ticks
-    svg.selectAll(".tick").style({"stroke-width":0});
-
-    // add the y Axis
-    var gy = svg.append("g")
-        .call(d3.axisLeft(y));
-
-    svg.selectAll(".domain").style({"stroke-width": 0});
-
-
-}
-
-
-var margin = { top: 50, right: 0, bottom: 100, left: 100 },
+    var margin = { top: 50, right: 0, bottom: 100, left: 100 },
     width = 400 - margin.left - margin.right,
     height = 530 - margin.top - margin.bottom,
     gridWidth = 90;
@@ -401,9 +209,6 @@ var margin = { top: 50, right: 0, bottom: 100, left: 100 },
         "exam4 no_exam": 12
     }
 
-var heatmapChart = function(csvFile) {
-
-    
     var div = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
@@ -443,8 +248,7 @@ var heatmapChart = function(csvFile) {
         clss += ((i >= 7 && i <= 16) ? "pretestLabel mono axis axis-workpretest" : "pretestLabel mono axis"); 
         return clss;    
     });
-
-            
+  
     d3.csv(csvFile,
     function(d) {
         return {
@@ -490,10 +294,15 @@ var heatmapChart = function(csvFile) {
                 elem.classed("selected-combo", false);
             } else {
                 elem.classed("selected-combo", true);
-                createMChoiceCircles(groupDataByChapterMChoice(["1","2"]));
+                var groups = ["1","2"];
+                var filtered_cohorts = cohortFilter(["1","2"]);
+                createTestBars(filtered_cohorts, groups, "mchoice", "#mChoiceCircles");
+                createTestBars(filtered_cohorts, groups, "parsons", "#parsonsCircles");
+                createTestBars(filtered_cohorts, groups, "unittest", "#unitTestCircles");
             }
         })
         .on("mouseover", function(d) {
+            console.log(d)
             div.transition()
                 .duration(200)
                 .style("opacity", .9);
